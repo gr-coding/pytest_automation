b01edef32ffa0fac3caaaf86585f25a25226469a
gr-coding <giri.rajaramv@outlook.com>
2025-03-05 17:57:45 +0000

    add_ui_tests

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -2,16 +2,13 @@ import pytest
 from pages.login_page import LoginPage
 
 
-
-
-@pytest.mark.parametrize("password", ["secret_sauce"])
 @pytest.mark.ui
-def test_login(driver, password):
+def test_login(driver):
     """
     Test to verify that login works with valid credentials.
     - Checks if the login is successful by verifying the URL.
     """
     login_page = LoginPage(driver)
-    login_page.login("standard_user", password)
+    login_page.login("standard_user", secret_sauce)
     get_page_url = driver.current_url
     assert "inventory" in get_page_url, "Login failed"
